c9d6d2c11fe3864b9db7b79c3ee007238c48fb96
liaojy <leoliao.email@gmail.com>
2026-06-13 14:43:57 +0800

    feat:洛谷顺序结构入门题

---
+++ practice/luogu/B2002.c
@@ -0,0 +1,10 @@
+//
+// Created by leoliao on 2026/6/13.
+//
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello,World!");
+    return 0;
+}
---
+++ practice/luogu/P1001.c
@@ -0,0 +1,14 @@
+//
+// Created by leoliao on 2026/6/13.
+//
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    scanf("%d %d", &a, &b);
+    printf("%d", a + b);
+
+    return 0;
+}
