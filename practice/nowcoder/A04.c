d824cfc84d8b0cbeebcf91dd30b5551b0df9f532
liaojy <leoliao.email@gmail.com>
2026-06-16 18:00:57 +0800

    feat: nowcoder basic practice

--- practice/nowcoder/A04.c
+++ practice/nowcoder/A04.c
@@ -1,3 +1,28 @@
 //
 // Created by leoliao on 2026/6/14.
+// https://www.nowcoder.com/practice/61a0f16117484d219b59c62224b33b1c
 //
+#include <stdio.h>
+
+int f(int x)
+{
+    if (x < 0)
+    {
+        return 1;
+    } else if (x == 0)
+    {
+        return 0;
+    } else
+    {
+        return -1;
+    }
+}
+
+int main()
+{
+    int x = 0;
+    scanf("%d",&x);
+    int y = f(x);
+    printf("%d\n", y);
+    return 0;
+}
