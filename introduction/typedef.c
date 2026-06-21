aee470e08728668ba12952f5d06bf52f96512ace
liaojy <leoliao.email@gmail.com>
2026-06-22 00:36:18 +0800

    feat: typedef static register keywords learn

---
+++ introduction/typedef.c
@@ -0,0 +1,19 @@
+//
+// Created by leoliao on 2026/6/21.
+// title: typedef
+//
+
+// typedef 顾名思义就是类型定义，这里应该理解成为类型重命名
+#include <stdio.h>
+
+
+typedef unsigned int uint;
+
+int main()
+{   
+    // unsigned 无符号的
+    unsigned int num = 0;
+    uint num2 = 0;
+    // 同一种类型 不同方式
+    return 0;
+}
